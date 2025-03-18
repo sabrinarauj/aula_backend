90599486918390d6395ac1dee83b98876ef9dce6
Sabrina Araújo <112494429+sabrinarauj@users.noreply.github.com>
2025-03-18 16:25:36 -0300

    Add files via upload

---
+++ listavariaveis.py
@@ -0,0 +1,126 @@
+""" 1 - nome = "João"
+print(nome) """
+
+""" 2 - a = 5
+b = 10
+
+soma = a + b
+print(soma)
+
+sub = a - b
+print(sub)
+
+mult = a * b
+print(mult)
+
+div = a/b
+print(div) """
+
+""" 3 - preco = 50
+desconto = 10
+
+precofinal = 50 - 10
+print(precofinal) """
+
+""" 4 -  resultado = 10 + (5*2)
+print(resultado) """
+
+""" 5 - texto = "150"
+int(texto)
+
+mult = 150 * 2
+print(mult) """
+
+""" 6 -  numeros = 1, 2, 3, 4, 5
+print(numeros) """
+
+""" print("Digite um valor: ")
+print("Digite outro valor: ") -> Voltar para essa questão novamente """
+
+""" x = input()
+y = input()
+
+print("Digite um valor x: ", x)
+print("Digite um valor y: ", y)
+
+div = x / y
+print(div) """
+
+""" 7 - n1 = int(input("Digite um valor: "))
+n2 = int(input("Digite outro valor: "))
+
+resultado = n1 + n2
+print(resultado) """
+
+""" 8 -  x = int(input("Digite um valor: \n"))
+y = int(input("Digite outro valor: \n"))
+
+divisao = x / y
+
+print(divisao) """
+
+""" 9 - num1 = int(input("Digite um valor: "))
+num2 = int(input("Digite outro valor: "))
+
+maior = num1 < num2 """
+
+""" 10 - idade = int(input("Digite sua idade: "))
+
+dias = idade * 365
+
+print("Você já viveu: ", dias) """
+
+""" 11- base = int(input("Digite o valor da base: "))
+expoente = int(input("Digite o valor do expoente: "))
+
+resultado = base ** expoente 
+
+print(resultado) """
+
+""" preco = float(input("Digite o preço: "))
+print("O preço é: ",type(str)) """
+
+""" raio = float(input("Digite o raio do círculo: "))
+
+area = raio * 3.14
+
+print(area) """
+
+""" a = int(input("Digite o valor de A: "))
+b = int(input("Digite o valor de B: "))
+
+a = b
+b = a
+
+print("Os novos valores são: ",a,b) """
+
+""" nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+media_ponderada = (nota1 * 2 + nota2 * 3 + nota3 * 5)/10
+print(media_ponderada) """
+
+""" import math
+
+x1 = float(input("Digite a coordenada x1: "))
+y1 = float(input("Digite a coordenada y1: "))
+x2 = float(input("Digite a coordenada x2: "))
+y2 = float(input("Digite a coordenada y2: "))
+
+distancia = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+
+print(distancia) """
+
+
+
+
+
+
+
+
+
+
+
+
+
